3f899b5d23a46172ada3cfbad6b6b7d9f2959be7
ramla02 <ramla2507@gmail.com>
2025-07-12 20:34:23 +0300

    basic operations and readme file

---
+++ python_introduction/README.md
@@ -0,0 +1,3 @@
+# Introduction to Python
+
+This project serves as a gentle introduction to the world of Python programming.
---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10
+number2 = 5
+add = number1 + number2
+sub = number1 - number2
+pro = number1 * number2
+
+print("Addition of", number1, "and", number2, "is", add)
+print("Subtraction of", number1, "and", number2, "is", sub)
+print("Multiplication of", number1, "and", number2, "is", pro)
