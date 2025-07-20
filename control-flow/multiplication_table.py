2b253c661b6fbdc02d05f89917b1134d15b399f7
ramla02 <ramla2507@gmail.com>
2025-07-20 21:50:44 +0300

    used f-string instead of comma

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,5 +1,5 @@
 number = int(input("Enter a number to see its multiplication table: "))
 for i in range(1, 11):
     product = number * i
-    print(number, "*", i, "=", product)
+    print(f"{number} * {i} = {product}")
 print()
