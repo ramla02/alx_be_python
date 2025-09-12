01910dfc29082846455c9cbbf1ceecaf335fc57e
ramla02 <ramla2507@gmail.com>
2025-09-12 11:08:35 +0300

    Arithmetic Operations Function

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,22 @@
+def perform_operation(num1: float, num2: float, operation: str):
+    
+    """
+    This program performs a basic  arithmetic operation between num1 and num2.
+    """
+    
+    if type(operation) is not str:
+        return "Error: operation must be string"
+    op = operation.strip().lower()
+    
+    if op == "add":
+        return num1 + num2
+    elif op == "subtract":
+        return num1 - num2
+    elif op == "multiply":
+        return num1 * num2
+    elif op == "divide":
+        if num2 == 0:
+            return "Error: Division by zero"
+        return num1 / num2
+    else:
+        return f"Error: Unknown operation '{operation}'"
